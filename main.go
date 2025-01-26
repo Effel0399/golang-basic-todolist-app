d9649da4b7827d34773988670e61e4283c6f9029
FNH99 <148479741+FNH99@users.noreply.github.com>
2025-01-26 19:46:56 +0700

    uploaded template

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var taskOne = "Watch Go crash course"
+	fmt.Println("##### Welcome to our Todolist App #####")
+	fmt.Println("List of Task:")
+	fmt.Println(taskOne)
+}
