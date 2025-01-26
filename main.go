a86bbba585dd831e6f7c694cc3bfa75116b2b4b5
FNH99 <148479741+FNH99@users.noreply.github.com>
2025-01-26 20:21:08 +0700

    indexing array with numbering and formatting

--- main.go
+++ main.go
@@ -8,22 +8,15 @@ func main() {
 	var task1 = "Watch Go crash course"
 	var task2 = "Take out trash"
 	var task3 = "Buy a donut"
-	// maxTask := 10
 
+	// basic array
 	var taskItems = [3]string{task1, task2, task3}
 
 	fmt.Println("##### Welcome to our Todolist App #####")
 
-	i := 0
-	taskCount := 1
-	for i <= 2 {
-		fmt.Print(taskCount)
-		fmt.Println(".", taskItems[i])
-		i++
-		taskCount++
-	}
-
-	for _, task := range taskItems {
-		fmt.Println(">", task)
+	// printing arrays with numbering
+	for index, task := range taskItems {
+		//fmt.Println(index+1, ".", task)
+		fmt.Printf("%d. %s\n", index+1, task)
 	}
 }
