d17cde52e1ebf84b74cba75d14fd8a5a3ef66caf
FNH99 <148479741+FNH99@users.noreply.github.com>
2025-01-27 12:52:31 +0700

    change variable scope and function

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 )
 
-var task1 = "Watch Go crash course"
-var task2 = "Take out trash"
-var task3 = "Buy a donut"
-
-// basic array
-var taskItems = []string{task1, task2, task3}
-
 func main() {
+	var task1 = "Watch Go crash course"
+	var task2 = "Take out trash"
+	var task3 = "Buy a donut"
+
+	// basic array
+	var taskItems = []string{task1, task2, task3}
+
 	fmt.Println("##### Welcome to our Todolist App #####")
-	printTasks()
+	printTasks(taskItems)
 }
 
-func printTasks() {
+func printTasks(taskItems []string) {
 	fmt.Println("List of Todos:")
 	// printing arrays with numbering
 	for index, task := range taskItems {
