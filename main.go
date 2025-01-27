e7c157ffa0f2aa78ffffe252bce454161f573441
FNH99 <148479741+FNH99@users.noreply.github.com>
2025-01-27 12:46:43 +0700

    change task to function printTasks, move variables to global

--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 )
 
+var task1 = "Watch Go crash course"
+var task2 = "Take out trash"
+var task3 = "Buy a donut"
+
+// basic array
+var taskItems = []string{task1, task2, task3}
+
 func main() {
-	var task1 = "Watch Go crash course"
-	var task2 = "Take out trash"
-	var task3 = "Buy a donut"
-
-	// basic array
-	var taskItems = [3]string{task1, task2, task3}
-
 	fmt.Println("##### Welcome to our Todolist App #####")
+	printTasks()
+}
 
+func printTasks() {
+	fmt.Println("List of Todos:")
 	// printing arrays with numbering
 	for index, task := range taskItems {
 		//format with printf
